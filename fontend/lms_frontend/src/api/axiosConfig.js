56fb6bec703537043055946994f5e4cc367a9345
Duckhal <hoangduckhai1922@gmail.com>
2025-12-30 22:07:01 +0700

    Fix wrong import

--- fontend/lms_frontend/src/api/axiosConfig.js
+++ fontend/lms_frontend/src/api/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { redirectToLogin } from '../utils/navigationHelper';
+import { redirectToLogin } from "./utils/navigationHelper";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || "";
 
